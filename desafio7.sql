6c3fef851570a7e7bd4437cd68bf3fdb594368fc
João Vitor Fischborn <joaovitorfischborn@icloud.com>
2022-09-26 17:01:56 -0300

    Requisito 07 ok corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -4,5 +4,5 @@ alb.nome_album AS album,
 COUNT(s.id_artista) AS seguidores
 FROM SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.albuns AS alb ON alb.id_artista = art.id_artista
-INNER JOIN SpotifyClone.seguidores AS s ON s.id_artista = alb.id_artista
+INNER JOIN SpotifyClone.seguindo AS s ON s.id_artista = alb.id_artista
 GROUP BY artista, album ORDER BY seguidores DESC, artista, album;
